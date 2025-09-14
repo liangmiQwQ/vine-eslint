410753e6e5063faa8f3a7b8446cec2257e9e17e4
liangmiQwQ <mi_yyds@126.com>
2025-09-14 12:38:47 +0800

    chore: update

--- src/app.vine.ts
+++ src/app.vine.ts
@@ -2,7 +2,10 @@ export function App() {
   const foo = (_some: string) => { }
   function functionFoo(_some: string) {}
 
-  function work(_some: string) { }
+  function work() { }
+  function work2() { }
+
+  function error() {}
 
   return vine`
     <div class="max-w-screen-lg mx-auto h-full px-12 py-24">
@@ -10,12 +13,12 @@ export function App() {
       <textarea @input="(e: Event) => foo((e.target as HTMLTextAreaElement).value)" />
       <textarea @input="(e: Event) => functionFoo((e.target as HTMLTextAreaElement).value)" />
 
-      <!-- ⬇️ Try to access an undefined method -->
-      <textarea @input="(e: Event) => bar((e.target as HTMLTextAreaElement).value)" />
-      <button @click="bar" />
-
       <!-- ✅ Seems only work expectedly with event like @click -->
       <button @click="work" />
+      <!-- ✅ Also work for simple arrow function -->
+      <button @click="() => work2()" />
+      <!-- ❌ Do not work with param -->
+      <button @click="(_e: any) => error()" />
     </div>
   `
 }
