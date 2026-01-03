7218734d54f5606f9092ebc080b9ffe980b52510
Liang Mi <github@liangmi.dev>
2026-01-03 22:06:39 +0800

    chore: update

--- src/app.vine.ts
+++ src/app.vine.ts
@@ -1,9 +1,7 @@
 export function App() {
-  function error() {}
-
   return vine`
-    <div class="max-w-screen-lg mx-auto h-full px-12 py-24">
-      <button @click="(_e: any) => error()" />
-    </div>
+    <pre>
+      {{ svg }}
+    </pre>
   `
 }
